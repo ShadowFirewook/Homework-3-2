b790f0298e383d1fff9ac1b43e4f63628f072d45
ShadowFirewook <104684219+ShadowFirewook@users.noreply.github.com>
2022-09-15 13:13:16 +0600

    Update First_Fragment.java

--- app/src/main/java/com/example/homework_3_2/First_Fragment.java
+++ app/src/main/java/com/example/homework_3_2/First_Fragment.java
@@ -51,5 +51,60 @@ public class First_Fragment extends Fragment {
         }
     });
 
+    }package com.example.homework_3_2;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class First_Fragment extends Fragment {
+
+    public Button buttonBig;
+    public Button buttonSmall;
+    public TextView textView;
+    public Integer counter;
+    public Integer digit;
+    public Integer sum;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_first_, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    Button buttonBig = view.findViewById(R.id.buttonBig);
+    Button buttonSmall = view.findViewById(R.id.buttonSmall);
+    TextView textView = view.findViewById(R.id.text);
+
+    buttonBig.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            digit = 1;
+            counter = Integer.parseInt(textView.getText().toString());
+
+            sum = digit + counter;
+            textView.setText(sum.toString());
+        }
+    });
+    buttonSmall.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            digit = 1;
+            counter = Integer.parseInt(textView.getText().toString());
+
+            sum = counter - digit;
+            textView.setText(sum.toString());
+        }
+    });
+
     }
 }
